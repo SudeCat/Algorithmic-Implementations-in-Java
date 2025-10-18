e14e0534668a1d36bd3eef8d7c9a3e0dca546f09
Sude <81020923+SudeCat@users.noreply.github.com>
2025-10-18 18:40:06 +0300

    Add files via upload
    huffman compression codes added.

---
+++ HuffmanCompression/src/HuffmanList.java
@@ -0,0 +1,89 @@
+public class HuffmanList {
+    public HuffmanNode head;
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public void sortedAdd(HuffmanNode node, HuffmanNode iterator, HuffmanNode prev){
+        if (iterator == null){
+            prev.next = node;
+            return;
+        }
+
+        if (node.frequency < iterator.frequency){
+            node.next = iterator;
+            prev.next = node;
+            return;
+        }
+
+        sortedAdd(node, iterator.next, prev.next);
+    }
+    public void sortedAdd(HuffmanNode node){
+        if (head == null){
+            head = node;
+            return;
+        }
+
+        if (head.frequency > node.frequency){
+            node.next = head;
+            head = node;
+            return;
+        }
+
+        sortedAdd(node, head.next, head);
+    }
+
+    public int count(){
+        int count = 0;
+
+        if (head == null){
+            return count;
+        }
+
+        HuffmanNode iterator = head;
+        while (iterator != null){
+            count++;
+            iterator = iterator.next;
+        }
+
+        return count;
+    }
+
+    public HuffmanNode popHead(){
+        if (isEmpty()){
+            return null;
+        }
+
+        HuffmanNode node = head;
+        head = this.head.next;
+        return node;
+    }
+
+    public void display(){
+        HuffmanNode iterator = head;
+
+        if (head == null){
+            System.out.println("Huffmanlist is empty");
+        }
+
+        while(iterator!=null){
+            System.out.print(iterator.symbol + " ");
+            iterator = iterator.next;
+        }
+
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        HuffmanList list = new HuffmanList();
+
+        list.sortedAdd(new HuffmanNode("a", 0.5f));
+        list.sortedAdd(new HuffmanNode("b", 0.4f));
+        list.sortedAdd(new HuffmanNode("c", 0.2f));
+        list.sortedAdd(new HuffmanNode("d", 0.3f));
+        list.sortedAdd(new HuffmanNode("e", 0.1f));
+
+        list.display();
+    }
+}
---
+++ HuffmanCompression/src/HuffmanTree.java
@@ -0,0 +1,90 @@
+public class HuffmanTree {
+    private HuffmanNode root;
+
+    public HuffmanTree(HuffmanList list){
+        HuffmanNode currentRoot;
+        while (true) {
+            HuffmanNode leftNode = list.popHead();
+            HuffmanNode rightNode = list.popHead();
+            currentRoot = new HuffmanNode(leftNode.symbol + rightNode.symbol, leftNode.frequency + rightNode.frequency);
+            currentRoot.left = leftNode;
+            currentRoot.right = rightNode;
+            if (list.isEmpty())
+                break;
+            list.sortedAdd(currentRoot);
+        }
+        root = currentRoot;
+    }
+
+    public String decode(String encodedSymbol){
+        String decodedSymbol = "";
+        HuffmanNode iterator = root;
+        for (char c : encodedSymbol.toCharArray()){
+            if (c == '0'){
+                iterator = iterator.left;
+            }
+            else {
+                iterator = iterator.right;
+            }
+            if (iterator.symbol.length() == 1){
+                decodedSymbol += iterator.symbol;
+                iterator = root;
+            }
+        }
+        return decodedSymbol;
+    }
+
+    public String encode(String symbol){
+        String encodedSymbol = "";
+        for (char c : symbol.toCharArray()){
+            String character = String.valueOf(c);
+
+            HuffmanNode iterator = root;
+            while(iterator != null){
+                if (iterator.symbol.length() == 1)
+                    break;
+                if (iterator.left != null && iterator.left.symbol.contains(character)){
+                    encodedSymbol += "0";
+                    iterator = iterator.left;
+                }
+                else{
+                    encodedSymbol += "1";
+                    iterator = iterator.right;
+                }
+            }
+        }
+        return encodedSymbol;
+    }
+
+    private void inorder(HuffmanNode parent){
+        if (parent == null)
+            return;
+
+        inorder(parent.left);
+        System.out.print(parent.symbol + " ");
+        inorder(parent.right);
+    }
+    public void inorder(){
+        inorder(root);
+        System.out.println();
+    }
+
+    public static void main(String[] args) {
+        HuffmanList list = new HuffmanList();
+        list.sortedAdd(new HuffmanNode("a", 0.35f));
+        list.sortedAdd(new HuffmanNode("b", 0.1f));
+        list.sortedAdd(new HuffmanNode("c", 0.2f));
+        list.sortedAdd(new HuffmanNode("d", 0.2f));
+        list.sortedAdd(new HuffmanNode("_", 0.15f));
+        list.display();
+
+        HuffmanTree tree = new HuffmanTree(list);
+        tree.inorder();
+
+        System.out.println(tree.encode("c"));
+        System.out.println(tree.encode("cdab"));
+
+        System.out.println(tree.decode("000110010111"));
+        System.out.println(tree.decode("0010110000"));
+    }
+}
